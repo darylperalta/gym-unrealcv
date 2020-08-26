1c6f40ac100685cd2aa66375d16705f44fe5af52
darylperalta <darylperalta@github.com>
2020-08-27 03:29:11 +0800

    min max in continuous

--- gym_unrealcv/envs/unrealcv_depth_keras_cont.py
+++ gym_unrealcv/envs/unrealcv_depth_keras_cont.py
@@ -12,17 +12,11 @@ from math import sin, cos, radians
 
 from gym_unrealcv.envs.utils.utils_depthFusion import write_pose, write_depth, depth_fusion, depth_conversion, poseRelToAbs, poseOrigin
 import time
-# import pcl
 import open3d as o3d
 o3d.set_verbosity_level(o3d.VerbosityLevel.Error)
-# import run_docker # a lib for run env in a docker container
-# import gym_unrealcv.envs.utils.run_docker
-
 import tensorflow as tf
 from tensorflow.python.framework import ops
 import keras.backend as K
-
-
 
 class depthFusion_keras_cont(gym.Env):
     # init the Unreal Gym Environment
@@ -35,8 +29,6 @@ class depthFusion_keras_cont(gym.Env):
                 observation_type = 'rgbd', # 'color', 'depth', 'rgbd'
                 reward_type = 'bbox', # distance, bbox, bbox_distance,
                 docker = False,
-                # resolution = (84,84)
-                # resolution = (640,480),
                 resolution = (640,480),
                 log_dir='log/'
     ):
@@ -45,26 +37,15 @@ class depthFusion_keras_cont(gym.Env):
      # self.reset_type = 'random'
      self.reset_type = 'test'
      self.log_dir = log_dir
-     # gt_pcl = pcl.load('house-000024-gt.ply')
-     # gt_pcl = pcl.load('/home/daryl/gym-unrealcv/BAT6_SETA_HOUSE44_OBJ_No_InnerMesh_sampled_10k.ply')
-     # gt_pcl = pcl.load('/home/daryl/datasets/BAT6_SETA_HOUSE8_WTR_sampled_10k.ply')
      gt_pcl = o3d.read_point_cloud('/home/daryl/gym-unrealcv/BAT6_SETA_HOUSE44_OBJ_No_InnerMesh_sampled_10k.ply')
      self.gt_pcl = np.asarray(gt_pcl.points, dtype=np.float32)
      # self.gt_pcl = np.asarray(gt_pcl)
      self.gt_pcl = np.expand_dims(self.gt_pcl,axis=0)
-     # run virtual enrionment in docker container
-     # self.docker = run_docker.RunDocker()
-     # env_ip, env_dir = self.docker.start(ENV_NAME=ENV_NAME)
 
      # start unreal env
      docker = False
-     # self.unreal = env_unreal.RunUnreal(ENV_BIN="house_withfloor/MyProject2/Binaries/Linux/MyProject2")
      self.unreal = env_unreal.RunUnreal(ENV_BIN=self.env_bin)
      env_ip,env_port = self.unreal.start(docker,resolution)
-
-     # connect unrealcv client to server
-     # self.unrealcv = UnrealCv(self.cam_id, ip=env_ip, env=env_dir)
-
 
      # connect UnrealCV
      self.unrealcv = Navigation(cam_id=self.cam_id,
@@ -89,11 +70,6 @@ class depthFusion_keras_cont(gym.Env):
 
      self.startpose = self.unrealcv.get_pose(self.cam_id)
 
-     #try hardcode start pose
-     # self.startpose = [750.0, 295.0, 212.3,356.5,190.273, 0.0]
-     # self.startpose = [0.0, 707.1068, 707.1067,0.0,270.0, -45.0] # [0,45,1000]
-     # self.startpose = [0.0,99.6,8.72,0.0,270.0,-5.0] #[for depth fusion] [0,5,100]
-     # self.startpose = [0.0,70.7,70.7,0.0,270.0,-45.0]
      azimuth, elevation, distance = self.start_pose_rel
      # print('start pose rel', azimuth,elevation,distance)
      self.startpose = poseRelToAbs(azimuth, elevation, distance)
@@ -102,15 +78,6 @@ class depthFusion_keras_cont(gym.Env):
      poseOrigin(self.log_dir+'frame-{:06}.pose.txt'.format(1000))
      # ACTION: (Azimuth, Elevation, Distance)
 
-
-     # ACTION: (linear velocity ,angle velocity)
-     # self.ACTION_LIST = [
-     #         (20,  0),
-     #         (20, 15),
-     #         (20,-15),
-     #         (20, 30),
-     #         (20,-30),
-     # ]
      self.count_steps = 0
      self.depth_count = 0
      self.depth_thresh = 0.5
@@ -118,71 +85,33 @@ class depthFusion_keras_cont(gym.Env):
      # self.max_steps = 35
      self.target_pos = ( -60,   0,   50)
      self.pose_prev = np.array(self.start_pose_rel)
-     # self.action_space = gym.spaces.Discrete(len(self.ACTION_LIST))
-     # state = self.unrealcv.read_image(self.cam_id, 'lit')
-     # self.observation_space = gym.spaces.Box(low=0, high=255, shape=state.shape)
+     self.nn_distance_module =tf.load_op_library(self.nn_distance_path)
 
-     self.nn_distance_module =tf.load_op_library('/home/daryl/gym-unrealcv/gym_unrealcv/envs/utils/tf_nndistance_so.so')
-     # if K.backend() == 'tensorflow':
-     #     config = tf.ConfigProto()
-     #     config.gpu_options.allow_growth = True
-     #     self.sess = tf.Session(config=config)
-     #     K.set_session(self.sess)
-     # self.sess = K.get_session()
-    # update the environment step by step
     def _step(self, action = 0):
-
-
         self.count_steps += 1
-        # collision =  self.unrealcv.move(self.cam_id, angle, velocity)
-        # collision = self.unrealcv.move_2d(self.cam_id, angle, velocity)
-        # collision = self.unrealcv.move_2d(self.cam_id, angle, velocity)
-        # azimuth, elevation, distance = action
-        # azimuth, elevation, distance = self.ACTION_LIST[action]
         azimuth, elevation, distance  = action
-        # print('az, elev, dist', azimuth, elevation, distance)
         change_pose = np.array((azimuth, elevation, distance))
 
         pose_prev = np.array(self.pose_prev)
-        # print('pose prev', pose_prev)
-        # print('action', change_pose)
-
-        MIN_elevation = 20
-        MAX_elevation = 70
-        MIN_distance = 100
-        MAX_distance = 150
-        # MAX_distance = 125
-
         pose_new = pose_prev + change_pose
-        # print('pose new 1', pose_new)
-        # pose_new = pose_prev + np.array([30,0,0]) # to test ICM
-        if pose_new[2] > MAX_distance:
-            pose_new[2] = MAX_distance
-        elif pose_new[2] < MIN_distance:
-            pose_new[2] = MIN_distance
-        if (pose_new[1] >= MAX_elevation):
-            pose_new[1] = MAX_elevation
-        elif (pose_new[1] <= MIN_elevation):
-            pose_new[1] = MIN_elevation
-        # else:
-        #     pose_new[1] = 45.0
+
+        if pose_new[2] > self.max_distance:
+            pose_new[2] = self.max_distance
+        elif pose_new[2] < self.min_distance:
+            pose_new[2] = self.min_distance
+        if (pose_new[1] >= self.max_elevation):
+            pose_new[1] = self.max_elevation
+        elif (pose_new[1] <= self.min_elevation):
+            pose_new[1] = self.min_elevation
+
         if (pose_new[0] < 0):
             pose_new[0] = 360 + pose_new[0]
         elif (pose_new[0]>=359.99):
             pose_new[0] = pose_new[0] - 360
 
-        # print('action ', action)
-        # print('pose new', pose_new)
-        # print(azimuth, elevation, distance )
-        # collision, move_dist = self.unrealcv.move_rel2(self.cam_id, azimuth, elevation, distance)
         collision, move_dist = self.unrealcv.move_rel2(self.cam_id, pose_new[0], pose_new[1], pose_new[2])
-        # print('collision', collision)
-        # print('distance:   ', move_dist)
-
         self.pose_prev = pose_new
-        # state = self.unrealcv.read_image(self.cam_id, 'lit')
         state = self.unrealcv.get_observation(self.cam_id, self.observation_type)
-        # print('state shape', state.shape)
         depth_pt = self.unrealcv.read_depth(self.cam_id,mode='depthFusion')
         pose = self.unrealcv.get_pose(self.cam_id,'soft')
         depth = depth_conversion(depth_pt, 320)
@@ -192,11 +121,9 @@ class depthFusion_keras_cont(gym.Env):
         np.save(depth_filename, depth)
         reward, done = self.reward(collision,move_dist)
 
-
         # limit max step per episode
         if self.count_steps > self.max_steps:
             done = True
-            # print('Reach Max Steps')
 
         return state, reward, done, {}
 
@@ -209,7 +136,6 @@ class depthFusion_keras_cont(gym.Env):
            distance = 1000
            azimuth = 0
            elevation = 45
-
            p=90
            distance = distance + random.randint(-250,250)
            azimuth = random.randint(0,359)
@@ -220,9 +146,7 @@ class depthFusion_keras_cont(gym.Env):
 
            y = distance*sin(radians(p-elevation))*cos(radians(azimuth))
            x = distance*sin(radians(p-elevation))*sin(radians(azimuth))
-
            z = distance*cos(radians(p-elevation))
-
            self.unrealcv.set_pose(self.cam_id,[x,y,z,0,yaw_exp,pitch]) # pose = [x, y, z, roll, yaw, pitch]
 
        else:
@@ -247,16 +171,10 @@ class depthFusion_keras_cont(gym.Env):
 
        return  state
 
-    # close docker while closing openai gym
-    # def _close(self):
-       # self.docker.close()
-
     # calcuate reward according to your task
     def reward(self,collision,move_dist):
 
-
        done = False
-
        depth_start = time.time()
        out_pcl_np = depth_fusion(self.log_dir, first_frame_idx =0, base_frame_idx=1000, num_frames = self.count_steps + 1, save_pcd = False, max_depth = 1.0)
        # print('out_pcl_np', out_pcl_np.shape)
@@ -268,28 +186,16 @@ class depthFusion_keras_cont(gym.Env):
        cd_delta = cd - self.cd_old
        depth_end = time.time()
 
-       # print("Depth Fusion time: ", depth_end - depth_start)
-       # print('coverage: ', cd)
-       # if cd > 30.0:
        if cd > 96.0:
-       # if cd > 60.0:
            done = True
            reward = 100
-            # print('covered', self.count_steps)
        else:
-           # reward = cd_delta*0.2
            reward = cd_delta
-           # reward = cd_delta*0.4
-           # reward += -0.2 * move_dist # added to push minimization of steps
-           # reward += -0.01 * move_dist
-           # reward += -0.02 * move_dist
            reward += -0.04 * move_dist
-           # print('move dist ', move_dist)
-           # print('cd delta', cd_delta)
+
        self.cd_old = cd
-       # print('reward ', reward)
        self.total_distance += move_dist
-       # print('total distance: ', self.total_distance)
+
        return reward, done
 
     # calcuate the 2D distance between the target and camera
@@ -321,6 +227,12 @@ class depthFusion_keras_cont(gym.Env):
        self.continous_actions = setting['continous_actions']
        self.env_bin = setting['env_bin']
        self.env_name = setting['env_name']
+       self.gt_fn = setting['pointcloud_path']
+       self.nn_distance_path = self.get_nn_distance(setting['nn_distance_path'])
+       self.min_elevation = setting['min_elevation']
+       self.max_elevation = setting['max_elevation']
+       self.min_distance = setting['min_distance']
+       self.max_distance = setting['max_distance']
        print('env name: ', self.env_name)
        print('env id: ', setting['env_bin'])
        return setting
@@ -330,56 +242,21 @@ class depthFusion_keras_cont(gym.Env):
        gympath = os.path.dirname(gym_unrealcv.__file__)
        return os.path.join(gympath, 'envs/setting', filename)
 
+    def get_nn_distance(self, filename):
+       import gym_unrealcv
+       gympath = os.path.dirname(gym_unrealcv.__file__)
+       return os.path.join(gympath, filename)
+
     def compute_chamfer(self, output):
-       # with tf.Session('') as sess:
-       # sess = K.get_session()
-       # self.sess.run(tf.global_variables_initializer())
-       # loss_out = self.sess.run(loss,feed_dict={inp_placeholder: output})
        with tf.device('/gpu:0'):
            sess = K.get_session()
            with sess.as_default():
-           # with tf.Session('') as sess:
-
-               # inp_placeholder = tf.placeholder(tf.float32)
-               # reta,retb,retc,retd=self.nn_distance(inp_placeholder,self.gt_pcl)
-               # with tf.name_scope('chamfer'):
-               # reta,retb,retc,retd=self.nn_distance(output,self.gt_pcl)
-               _,_,retc,_=self.nn_distance(output,self.gt_pcl)
-               # loss=tf.reduce_sum(reta)+tf.reduce_sum(retc)
-
-               # loss=tf.reduce_sum(retc)
+               _,_,retc,_ = self.nn_distance(output,self.gt_pcl)
                dist_thresh = tf.greater(0.0008, retc)
                dist_mean = tf.reduce_mean(tf.cast(dist_thresh, tf.float32))
-
-               # loss_out = tf.Tensor.eval(loss)
                coverage = tf.Tensor.eval(dist_mean)
-               #
-               #
-               # dist_thresh2 = tf.greater(0.0008, reta)
-               # dist_mean2 = tf.reduce_mean(tf.cast(dist_thresh2, tf.float32))
-               #
-               # coverage2 = tf.Tensor.eval(dist_mean2)
-               # print('coverage2 ', coverage2)
-               # loss_out = self.sess.run(loss,feed_dict={inp_placeholder: output})
-               # print('coverage ', coverage)
+
                return coverage*100
-
-    # def nn_distance_init(self, output):
-
-
-       #@tf.RegisterShape('NnDistance')
-       #def _nn_distance_shape(op):
-        #shape1=op.inputs[0].get_shape().with_rank(3)
-        #shape2=op.inputs[1].get_shape().with_rank(3)
-        #return [tf.TensorShape([shape1.dims[0],shape1.dims[1]]),tf.TensorShape([shape1.dims[0],shape1.dims[1]]),
-            #tf.TensorShape([shape2.dims[0],shape2.dims[1]]),tf.TensorShape([shape2.dims[0],shape2.dims[1]])]
-       # @ops.RegisterGradient('NnDistance')
-       # def _nn_distance_grad(op,grad_dist1,grad_idx1,grad_dist2,grad_idx2):
-       #     xyz1=op.inputs[0]
-       #     xyz2=op.inputs[1]
-       #     idx1=op.outputs[1]
-       #     idx2=op.outputs[3]
-       #     return nn_distance_module.nn_distance_grad(xyz1,xyz2,grad_dist1,idx1,grad_dist2,idx2)
 
     def nn_distance(self,xyz1,xyz2):
        '''
